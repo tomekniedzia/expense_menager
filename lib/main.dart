ccaab3de268944e6398a547906d38ebf07f503c8
Tomnieee <tomekniedzia@gmail.com>
2022-04-27 21:25:41 +0200

    Add first layout

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,22 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: transaction.map((tx) {
                 return Card(
-                  child: Text(tx.title),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          child: Text(
+                        tx.amount.toString(),
+                      )),
+                      Column(
+                        children: <Widget>[
+                          Text(tx.title),
+                          Text(
+                            tx.date.toString(),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }).toList(),
             )
